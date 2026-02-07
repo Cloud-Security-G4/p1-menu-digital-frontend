cd115e14ae9351059a81c6b9289eacb6914ebe6d
LindsayPinto <l.pintom@uniandes.edu.co>
2026-02-07 16:18:04 -0500

    feat: Initialize frontend project with React, Vite and TailwindCSS and add README

---
+++ livemenu-front/src/App.tsx
@@ -0,0 +1,14 @@
+
+import './App.css'
+
+function App() {
+  return (
+    <div className="flex items-center justify-center h-screen">
+      <h1 className="text-4xl font-bold text-green-500">
+        Configuración inicial 
+      </h1>
+    </div>
+  )
+}
+
+export default App
